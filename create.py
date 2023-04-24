c9f2212617903aa29642653afed4c8d32315dbfa
anita3248 <131422054+anita3248@users.noreply.github.com>
2023-04-24 18:36:16 -0400

    Update create.py

--- create.py
+++ create.py
@@ -8,5 +8,20 @@ def login(username, password):
 
 
 def regiser(username, password):
+    # create_username_password.py
+
+users = {} # initialize an empty dictionary to store user data
+
+def create_account():
+    username = input("Enter a username: ")
+    password = input("Enter a password: ")
+    users[username] = password # add the username and password to the users dictionary
+    print("Account created successfully!")
+
+create_account() # call the function to create a new account
+
+# print the dictionary to check the data
+print(users)
+
     #Write username and password to file, make sure credentials arent already used.
     pass
